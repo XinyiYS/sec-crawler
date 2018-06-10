13f7e9a373fc4f7afdbea497a870164083116011
X <michaelxysg@gmail.com>
2018-06-10 13:31:22 +0800

    miscellaneous files

---
+++ sec v0.6/app 10Q.py
@@ -0,0 +1,242 @@
+import random,time,itertools,threading,re,datetime
+import urllib,csv,os, errno, wget
+from re import search
+from urllib.parse import quote,unquote
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing import Process
+import shutil
+import IndexDownloader
+import numpy as np
+from math import ceil
+import pandas as pd 
+from bs4 import BeautifulSoup
+import requests
+
+
+def compile_logs(log_folder,interval_update=False):
+	if not os.path.isdir(log_folder):
+		return [0]
+
+	logs = [ (os.path.join(log_folder, logname)) for logname in os.listdir(log_folder) if logname.endswith('.log')]
+
+	rows = []
+	for log in logs:
+		with open(log) as f:
+			rows.extend(f.read().strip().split(' '))
+
+	with open((os.path.join(log_folder, "aggregate log")),'a') as f:
+			f.write(" "+" ".join(rows)+" ")
+
+	with open((os.path.join(log_folder, "aggregate log")),'r') as agg_log:
+		rows = agg_log.read().strip().split(' ')
+
+	rows = np.array(np.unique([int(row) for row in rows if row.isdigit()]))
+	return rows
+
+def clear_logs(log_folder):
+	if not os.path.isdir(log_folder):
+		return 
+	try:
+		os.remove(log_folder+"/aggregate log")
+		return
+	except:
+		return
+
+def update_database(log_folder,database_path):
+	if not (os.path.isdir(log_folder) and os.path.exists(database_path)):
+
+		if os.path.exists(database_path):
+			df = pd.read_csv(database_path,chunksize = 100000)
+			total = sum([len(chunk) for chunk in df])
+			return 0, total
+		else:
+			return 0,0
+
+	rows = compile_logs(log_folder)
+
+	try:
+		df = pd.read_csv(database_path)
+		df.loc[rows,'download'] = 1
+		total = len(df)
+		completed = df['download'].sum()
+		updated_database = database_path[:-4]+"-copy.csv"
+		df.to_csv(updated_database,index=False)
+		os.remove(database_path)
+		os.rename(updated_database,database_path)
+		print("{}/{} filings have been downloaded: {:.2%} complete. Continuing from last download. \n".format(str(completed), str(total), (completed/total) ))
+		clear_logs(log_folder)
+	except:
+		print("An error occured at the update, please redo this step.")
+		return
+
+	[os.remove((os.path.join(log_folder, logname))) for logname in os.listdir(log_folder) if logname.endswith('.log')] # remove all the redundant logs
+	return completed, total
+
+
+def create_folder(folder_name):
+	try:
+		os.makedirs(folder_name)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
+	return
+
+def get_urls(url):
+	soup = BeautifulSoup(requests.get(url).text,'lxml')
+	return [link.get('href') for  link in soup.find_all('a') if link.get('href').startswith('/Archives/edgar/data/')] # COMMON_PREFIX = '/Archives/edgar/data/'
+
+# single download
+def get_file(filing_folder_path,url):
+	url= "".join(["https://www.sec.gov",url])
+	filename = url.split('/')[-1]
+	wget.download(url , filing_folder_path + '/'+ filename)
+	return (url+"\n")
+
+
+def download_data_chunk(chunk):
+
+	log_folder = "Download Logs"
+	create_folder(log_folder)
+	datafolder = "Downloaded data files"
+
+	log_name = "{}-{}-download.log".format(str(chunk.index[0]),str(chunk.index[-1]))
+
+	process,download_entire,iotime = [],[],[]
+	counter = 0
+	with open(os.path.join(log_folder,log_name),"a") as log:
+		for row_index, row in chunk[chunk['download']==0].iterrows():
+			stamps = []
+			
+			# stamps[0]
+			stamps.append(time.time())
+			conm = row['conm']
+			htm = row['path']
+			filing = row['type']
+			cik = row['cik']
+			date = row['date']
+			date_str = "".join(str(date).split("-"))
+			yr_qtr = "{}-QTR{}".format(date[:4],ceil(int(date[5:7])/3))
+
+			if ('/' in filing):
+				filing = quote(filing, safe='') # use percent encoding to escape the slash # use urllib.parse.unquote(encoded_str,'utf8') to decode
+			filing_folder_name = ' '.join([str(row_index),str(cik),str(date_str),str(filing)])
+			filing_folder_path = '/'.join([datafolder, yr_qtr , filing_folder_name])
+
+
+			# print('Start fetching URL to', conm, filing, 'filed on', date, '...')
+			# start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+			
+			create_folder(filing_folder_path)
+
+			urls = get_urls(htm)
+
+			# stamps[1]
+			stamps.append(time.time())
+
+			try:
+
+				pool = ThreadPool(len(urls)) # instantiate multiple threads
+				all_urls = (htm+"\n") + "".join(pool.starmap(get_file, zip(itertools.repeat(filing_folder_path), urls)))
+				pool.close() 
+				pool.join() # wait for all to finish
+				# stamps[2]
+				stamps.append(time.time())
+
+				with open(filing_folder_path+'/'+'download.log', 'w', newline='') as download_log:
+					download_log.write(all_urls)
+
+				log.write(str(row_index)+" ")
+				log.flush()
+
+				# stamps[3]
+				stamps.append(time.time())
+				
+				process.append(stamps[1]-stamps[0])
+				download_entire.append(stamps[2]-stamps[1])
+				iotime.append(stamps[3]-stamps[2])
+				
+			except:
+				print("Error in downloading this file.",htm)
+
+			counter+=1
+			if( counter % 100==0):
+				# timelog.write("For {} filings. Process avg: {:.3f} seconds. Latency avg: {:.3f} seconds. Iotime avg: {:.3f}".format(str(counter),np.mean(process),np.mean(latency),np.mean(iotime)))
+				print("For {} filings. Process avg: {:.3f} seconds. Documents downloading avg: {:.3f}. \
+					Iotime avg: {:.3f}".format(str(counter),np.mean(process), np.mean(download_entire),np.mean(iotime)))
+
+	return
+
+
+# using threadPool, inherently multithreaded
+def start_download(database,n_threads=9):
+
+	log_folder = "Download logs"
+
+	pool = ThreadPool(n_threads) # instantiate multiple threads
+
+	df = pd.read_csv(database,chunksize=10000)
+	pool.map(download_data_chunk, df) # run the threads	
+	pool.close() 
+	pool.join() # wait for all to finish
+	
+	update_database(log_folder ,database)
+
+	print("all done.")
+	return
+
+def update(start,interval,log_folder,count,total):
+
+	log_folder = "Download logs"
+	no_intervals = 0
+
+	while(True):
+		end = time.time()
+		seconds_elapsed = end - start
+		if seconds_elapsed > (no_intervals+1) * interval:
+
+			rows = compile_logs(log_folder,interval_update=True)
+			completed = len(rows)
+			eta = int(seconds_elapsed / (completed/total))
+			eta_str = datetime.timedelta(seconds=eta)
+			total_completed = completed + count
+
+			print("This program has run for {:.2f} hours, and downloaded {} filings, at a rate of {:.2f} filings per second.".format((seconds_elapsed/3600),
+			str(completed), (completed/seconds_elapsed)))
+			print("{}/{} filings have been downloaded: {:.2%} complete. Estimated time for completion: {}.\n".format(str(total_completed), str(total), (total_completed/total), eta_str))
+			no_intervals+=1
+
+
+if __name__ == '__main__':
+
+	year = 2018
+	print("Preparing database...")
+	database_path = "Database files/{}-QTR1 to 2018-QTR2.csv".format(str(year))
+	if not os.path.exists(database_path):
+		database = IndexDownloader.download_and_convert(year=year)
+	else:
+		database = database_path
+	print("Database preparation successful.")
+
+	PREFIX = "https://www.sec.gov/Archives/edgar/data/" # common prefix for data files
+
+	datafolder = "Downloaded data files"
+	create_folder(datafolder)
+
+	log_folder = "Download logs"
+
+	print("\nStart downloading filings. This will take a while...\n")
+	count, total = update_database(log_folder ,database)
+
+	start_time = time.time()
+	n_threads = 9
+	update_p1 = Process(target=update,args=(start_time,60,log_folder,count,total))
+	download_p2 = Process(target=start_download,args=(database,n_threads,))
+	download_p2.start()
+	update_p1.start()
+	download_p2.join()
+	update_p1.terminate()
+	print("\n all processed terminated")
+	print("\nDownloading complete.")
+	exit()
+
