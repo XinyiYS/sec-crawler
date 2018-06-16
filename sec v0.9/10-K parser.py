7ad4693cf82d35e98f92adf90a9e12c4e7d14dfb
X <michaelxysg@gmail.com>
2018-06-16 15:05:39 +0800

    Added a remove duplicate helper python

---
+++ sec v0.9/10-K parser.py
@@ -0,0 +1,96 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib
+import re
+
+url = "https://www.sec.gov/Archives/edgar/data/1652044/000165204418000007/goog10-kq42017.htm"
+def get_urls(url):
+	soup = BeautifulSoup(requests.get(url).text,'lxml')
+	return [link.get('href') for  link in soup.find_all('a') if link.get('href').startswith('/Archives/edgar/data/')] # COMMON_PREFIX = '/Archives/edgar/data/'
+
+
+# soup = BeautifulSoup (open("pih-10k_123116.htm"),'lxml')
+# content = soup.find_all("a",href=False)
+# print(len(content))
+# print(content[0]['name'])
+def get_delim_text(soup):
+	text = soup.get_text()
+	# break into lines and remove leading and trailing space on each
+	lines = (line.strip() for line in text.splitlines())
+	# break multi-headlines into a line each
+	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+	# drop blank lines
+	text = '\n'.join(chunk for chunk in chunks if chunk)
+	return text
+
+from html.parser import HTMLParser
+
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
+    def handle_endtag(self, tag):
+        print("End tag  :", tag)
+    def handle_data(self, data):
+        print("Data     :", data)
+    def handle_comment(self, data):
+        print("Comment  :", data)
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+    def handle_decl(self, data):
+        print("Decl     :", data)
+
+soup = BeautifulSoup (open("form10k.htm"),'lxml')
+parser = MyHTMLParser()
+parser.feed(soup.prettify())
+
+#printing the extracted values
+# print("Start tags", parser.lsStartTags)
+# print("End tags", parser.lsEndTags)
+# print("Start End tags", parser.lsStartEndTags)
+# print("Comments", parser.lsComments)
+exit()
+
+
+content = soup.prettify()
+links = soup.find_all("a",href=False)
+names = [link['name'] for link in links]
+name = names[2]
+
+print(name)
+begin = content.find(name)
+end = content.rfind(name)
+print(begin,end+len(name))
+print("*************************")
+a= BeautifulSoup(content[begin:end],'lxml')
+print(get_delim_text(a))
+exit()
+
+
+
+counts = [content.count(name) for name in names]
+
+print(counts)
+print(names)
+
+# result = re.findall("PART I.*?PART II", content, flags=re.S)
+# print(result)
+# print(result.group(0))
+
+
+
+
+
+
+
---
+++ sec v0.9/rm_duplicates.py
@@ -0,0 +1,48 @@
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing import Process
+from bs4 import BeautifulSoup
+import requests
+from os import walk
+import os,re
+
+def get_filing_folders(folder_to_clean,filing = '10-K'):
+	return [dirpath for (dirpath, dirnames, filenames) in walk(folder_to_clean)
+					if len(dirpath.split("/")) == 4] # check if windows compatible
+
+def get_urls(url):
+	soup = BeautifulSoup(requests.get(url).text,'lxml')
+	return [link.get('href') for  link in soup.find_all('a') if link.get('href').startswith('/Archives/edgar/data/')] 
+
+def remove_duplicates(dirpath):
+	download_log = "download.log"
+	if(not os.path.exists(os.path.join(dirpath,download_log))): # download still not complete
+		return
+	with open(os.path.join(dirpath,download_log),'r') as referrence:
+		lines = referrence.readlines()
+	files = os.listdir(dirpath)
+	if(len(lines)-1 == len(files)-1):   # lines include all files and one master link, files include all files and one log
+		# Nothing to remove
+		return
+	else:
+		# htm = lines[0]
+		# urls = get_urls(htm)
+		# filenames = [url.split('/')[-1] for url in urls] # actual necessary files
+		filenames = [url.split('/')[-1] for url in get_urls(lines[0])]
+		filenames.append("download.log") # include download.log, NOT to be removed
+		[os.remove(os.path.join(dirpath,file)) for file in files if file not in filenames]
+	return
+
+def clean_folder(folder_to_clean):
+	dirpaths = get_filing_folders(folder_to_clean)
+	[remove_duplicates(dirpath) for dirpath in dirpaths]
+	# cleaners = 5
+	# pool = ThreadPool(len(cleaners)) # instantiate multiple threads
+	# pool.starmap(remove_duplicates, dirpaths)
+	# pool.close() 
+	# pool.join() # wait for all to finish
+	print("all subdirectories are removed of duplicates")
+	return
+
+folder_to_clean = "Downloaded data files"
+clean_folder(folder_to_clean)
